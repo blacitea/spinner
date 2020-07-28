cb92507749db19d138c7c31219a2f9de92b48f5d
Angel Tsang <blacitea@gmail.com>
2020-07-28 21:56:20 +0000

    end up with a for-loop, setinterval was a wrong path

---
+++ spinner2.js
@@ -0,0 +1,77 @@
+// let loop = 0;
+// let time = 100;
+// while (loop < 2) {
+//   setTimeout(() => {
+//     process.stdout.write('\r|   ');
+//   }, time);
+//   setTimeout(() => {
+//     process.stdout.write('\r/   ');
+//   }, time + 200);
+//   setTimeout(() => {
+//     process.stdout.write('\r-   ');
+//   }, time + 400);
+//   setTimeout(() => {
+//     process.stdout.write('\r\\   ');
+//   }, time + 600);
+//   loop++;
+//   time += 700;
+// }
+
+
+let outterLoop = 100;
+let time = 100;
+let loop = 0;
+let frames = ["|", "/", "-", "\\"];
+
+for (let i = 0; i < 9; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r ${frames[i % 4]}     ${i === 8 ? "\n" : ""}`);
+  }, 200 * i);
+}
+
+
+
+// // Looping HELL
+// let anime = setInterval(() => {
+//   for (let each of frame) {
+//     setTimeout(() => {
+//       process.stdout.write(`\r ${each}     `);
+//       loop++;
+//       if (loop > 8) {
+//         process.stdout.write(`\r  |     `);
+//         console.log("Loop hits 8 here");
+//         clearInterval(anime);
+//       }
+//     }, time);
+//     time += 200;
+//   }
+// }, time);
+
+// for (let j = 0; j <= 1; j++) {
+//   for (let i = 0; i < frames.length; i++) {
+//     console.log((200 * i) + (j * frames.length * 200));
+//     setTimeout(() => process.stdout.write(`\r ${frames[i]}     `), ((200 * i) + (j * frames.length * 200)));
+//     // if (j ==== 1 && i ==== 3) {
+//     //   process.stdout.write(`\r |     `)
+//     // }
+//   }
+// }
+
+// make a loop that runs twice
+//   in each loop, loop through frame
+// inside frame loop, each do a timeout call of char (with time incremented)
+
+// let anime = setInterval(() => {
+//   for (let each of frame) {
+//     setTimeout(() => {
+//       process.stdout.write(`\r ${each}     `);
+//       loop++;
+//       if (loop > 8) {
+//         process.stdout.write(`\r  |     \n`);
+//         clearInterval(anime);
+//       }
+//     }, time);
+//     time += 200;
+//   }
+//   clearInterval(anime);
+// }, time);
