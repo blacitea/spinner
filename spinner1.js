f1552aae03a00ddefe5a9bd1bb60b9736d89d90b
Angel Tsang <blacitea@gmail.com>
2020-07-28 20:14:45 +0000

    trying out the example

--- spinner1.js
+++ spinner1.js
@@ -1,6 +1,23 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
-process.stdout.write("I want to say Hi to my Mimi");
+// process.stdout.write("I want to say Hi to my Mimi");
+// setTimeout(() => {
+//   process.stdout.write("\rI want to say I love him\n");
+// }, 1000);
+
 setTimeout(() => {
-  process.stdout.write("\rI want to say I love him\n");
-}, 1000);
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
